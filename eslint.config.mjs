a5f9094f7559302cad59f23bbeb320426d49f353
garg-lakshay <lakshaygarg775@gmail.com>
2025-11-28 01:20:29 +0530

    fixed eslint issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,20 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import tseslint from "typescript-eslint";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  
+  tseslint.config({
+    rules: {
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/use-unknown-in-catch-callback-variable": "error"
+    }
+  }),
+
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
